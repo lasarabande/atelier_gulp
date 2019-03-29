91a69b438fca8febed8cc30e18c0d50b4c686ac2
PA5 <PA5@ITF5DM.adds>
2019-03-29 12:18:09 +0100

    amélioration du commentaire concernant sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function(){
-   console.log('Salut');
+   console.log('Transformation de SCSS en CSS');
     
     return gulp.src('dev/scss/styles.scss')
         .pipe(sass())
